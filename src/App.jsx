37be52f4aa0948cd6cf1d37791bb047f482c1829
Salah SAS <salahaznidi09@gmail.com>
2024-12-22 15:45:09 +0100

    Add spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ClipLoader } from "react-spinners"; // Importer le spinner
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Slider from "./components/Slider";
-import "./index.css"; // Fichier CSS global
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import CvVideo from "./components/CvVideo";
+import "./index.css"; // Fichier CSS global
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.onload = () => {
+      setLoading(false); // Le chargement est terminé, on cache le loader
+    };
+  }, []);
+
   return (
     <>
-      
-      <Navbar />
-      <Hero />
-      <CvVideo />
-      <Slider />
-      <About />
-      <Contact />
-      <Projects />
-      <Footer />
+      {loading ? (
+        <div
+          className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+        >
+          <ClipLoader size={50} color={"#fbbf24"} loading={loading} />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <CvVideo />
+          <Slider />
+          <About />
+          <Contact />
+          <Projects />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
